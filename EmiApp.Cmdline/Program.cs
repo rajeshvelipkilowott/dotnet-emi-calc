cd1f27e0249fb4a2ac85d8037eb24a5bbdd547c7
rajeshvelipkilowott <92842921+rajeshvelipkilowott@users.noreply.github.com>
2021-12-07 00:02:26 +0530

    Add files via upload

---
+++ EmiApp.Cmdline/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace EmiApp.Cmdline
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var emiRequest = new Request() { Principal = 100000, InterestRateInPercentage = 12, LoanDurationInYearCount = 2 };
+            ICalculator emiCalculator = new Calculator();
+            Console.WriteLine(emiRequest);
+            #region monthly compounding
+            var emiMonthly = emiCalculator.CalculateMonthlyEmi(emiRequest);
+            if (string.IsNullOrEmpty(emiMonthly.ErrorMessage))
+                Console.WriteLine($"Monthly EMI is {emiMonthly.EmiPayment}");
+            else 
+                Console.WriteLine(emiMonthly.ErrorMessage);
+            #endregion 
+
+            var emiDaily = emiCalculator.CalculateDailyEmi(emiRequest);
+            if (string.IsNullOrEmpty(emiDaily.ErrorMessage))
+                Console.WriteLine($"Daily EMI is {emiDaily.EmiPayment}");
+            else
+                Console.WriteLine(emiDaily.ErrorMessage);
+
+            var emiContinous = emiCalculator.CalculateContinousEmi(emiRequest);
+            if (string.IsNullOrEmpty(emiContinous.ErrorMessage))
+                Console.WriteLine($"Continous EMI is {emiContinous.EmiPayment}");
+            else
+                Console.WriteLine(emiContinous.ErrorMessage);
+
+            Console.ReadLine();
+        }
+    }
+}
+
