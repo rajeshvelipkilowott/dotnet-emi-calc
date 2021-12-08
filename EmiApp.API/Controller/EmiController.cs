22c148a210571a6036c6ce716caa5cf269fc5506
rajeshvelipkilowott <92842921+rajeshvelipkilowott@users.noreply.github.com>
2021-12-08 11:50:21 +0530

    Add files via upload

---
+++ EmiApp.API/Controller/EmiController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmiApp.API.Controllers
+{
+    
+    [ApiController]
+    public class EmiController : ControllerBase
+    {
+        [HttpPost]
+        [Route("api/[controller]/CalculateMonthlyEmi")]
+        //[HttpPost(Name = "CalculateMonthlyEmi")]
+        public Response CalculateMonthlyEmi(Request request)
+        {
+            ICalculator emiCalculator = new Calculator();
+            var response = new Response();
+            var emiMonthly = emiCalculator.CalculateMonthlyEmi(request);
+            return emiMonthly;
+        }
+        //[HttpPost(Name = "CalculateDailyEmi")]
+        [HttpPost]
+        [Route("api/[controller]/CalculateDailyEmi")]
+        public Response CalculateDailyEmi(Request request)
+        {
+            ICalculator emiCalculator = new Calculator();
+            var response = new Response();
+            var emiDaily = emiCalculator.CalculateDailyEmi(request);
+            return emiDaily;
+        }
+        //[HttpPost(Name = "CalculateContinousEmi")]
+        [HttpPost]
+        [Route("api/[controller]/CalculateContinousEmi")]
+        public Response CalculateContinousEmi(Request request)
+        {
+            ICalculator emiCalculator = new Calculator();
+            var response = new Response();
+            var emiContinous = emiCalculator.CalculateContinousEmi(request);
+            return emiContinous;
+        }
+    }
+}
