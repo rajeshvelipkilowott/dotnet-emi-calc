368ae9e2ad72ca4d66b1ffe2c2ba0636eed24c88
rajeshvelipkilowott <92842921+rajeshvelipkilowott@users.noreply.github.com>
2021-12-09 23:55:31 +0530

    Add files via upload

---
+++ RestEmiApp/Controllers/DailyEmiController1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class DailyEmiController1 : Controller
+    {
+        public IActionResult Output(double principal, double rate, double duration)
+        {
+            double Result;
+            Request request = new Request();
+            request.Principal = principal;
+            request.InterestRateInPercentage = rate;
+            request.LoanDurationInYearCount = duration;
+            Result = (double)request.Principal * System.Math.Pow(1 + ((request.InterestRateInPercentage / 100) / 365), 365 * request.LoanDurationInYearCount) - request.Principal;
+            request.Result = Math.Round(Result, 2);
+            return View(request);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult EmiDaily()
+        {
+            return View();
+        }
+    }
+}
